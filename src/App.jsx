d0b06b5b14a6da5da9067a9bd19f8fcda7a36d0b
ilhamfirdaa <ilhamfirdaa@gmail.com>
2026-08-11 18:04:26 +0700

    hide MusicToggle on /guest-list page

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { BrowserRouter, Routes, Route, useLocation } from 'react-router-dom'
 import { Suspense, lazy } from 'react'
 import { Toaster } from 'react-hot-toast'
 import { AudioProvider } from './context/AudioContext'
@@ -9,20 +9,31 @@ const GuestCheckIn = lazy(() => import('./pages/GuestCheckIn'))
 const AdminCheckin = lazy(() => import('./pages/AdminCheckin'))
 const GuestList = lazy(() => import('./pages/GuestList'))
 
+function AppContent() {
+  const location = useLocation()
+  const hideMusicToggle = location.pathname === '/guest-list'
+
+  return (
+    <>
+      <Toaster position="top-center" toastOptions={{ duration: 3000 }} />
+      <Suspense fallback={null}>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/guest/:id" element={<GuestCheckIn />} />
+          <Route path="/admin/checkin" element={<AdminCheckin />} />
+          <Route path="/guest-list" element={<GuestList />} />
+        </Routes>
+      </Suspense>
+      {!hideMusicToggle && <MusicToggle />}
+    </>
+  )
+}
+
 export default function App() {
   return (
     <AudioProvider>
       <BrowserRouter>
-        <Toaster position="top-center" toastOptions={{ duration: 3000 }} />
-        <Suspense fallback={null}>
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/guest/:id" element={<GuestCheckIn />} />
-            <Route path="/admin/checkin" element={<AdminCheckin />} />
-            <Route path="/guest-list" element={<GuestList />} />
-          </Routes>
-        </Suspense>
-        <MusicToggle />
+        <AppContent />
       </BrowserRouter>
     </AudioProvider>
   )
