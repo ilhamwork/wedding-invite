39166c0254ba6c432b23a2edfbef8b55b6aa15db
ilhamfirdaa <ilhamfirdaa@gmail.com>
2026-08-11 21:33:48 +0700

    update migrations om ilham & tante dian with phone numbers, upsert on conflict

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import { Suspense, lazy } from 'react'
 import { Toaster } from 'react-hot-toast'
 import { AudioProvider } from './context/AudioContext'
-import { Analytics } from "@vercel/analytics/next"
 import Home from './pages/Home'
 
 const GuestCheckIn = lazy(() => import('./pages/GuestCheckIn'))
@@ -30,7 +29,6 @@ export default function App() {
     <AudioProvider>
       <BrowserRouter>
         <AppContent />
-        <Analytics />
       </BrowserRouter>
     </AudioProvider>
   )
